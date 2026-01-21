82b74050ca16a80260c8944cc87427eb8b3c3518
XHxinhe <1539210279@qq.com>
2026-01-22 01:27:43 +0800

    修复了古式地图不显示云杉木树图标的问题

--- src/main/java/hunternif/atlas/ext/ExtBiomeDataHandler.java
+++ src/main/java/hunternif/atlas/ext/ExtBiomeDataHandler.java
@@ -1,11 +1,27 @@
 package hunternif.atlas.ext;
 
+import hunternif.atlas.api.AtlasAPI;
+import hunternif.atlas.client.TextureSet;
+import net.minecraft.src.BiomeGenBase;
 import net.minecraft.src.EntityPlayer;
 import net.minecraft.src.World;
 
 public class ExtBiomeDataHandler {
     private static final String DATA_KEY = "aAtlasExtTiles";
     private ExtBiomeData data;
+
+    public ExtBiomeDataHandler() {
+        registerVanillaBiomes();
+    }
+
+    private void registerVanillaBiomes() {
+
+
+        // 针叶林 -> 松树
+        AtlasAPI.getTileAPI().setBiomeTexture(BiomeGenBase.taiga.biomeID, TextureSet.PINES);
+        AtlasAPI.getTileAPI().setBiomeTexture(BiomeGenBase.taigaHills.biomeID, TextureSet.PINES_HILLS);
+
+    }
 
     public void onWorldLoad(World world) {
         if (!world.isRemote) {
@@ -16,7 +32,6 @@ public class ExtBiomeDataHandler {
                 world.setItemData("aAtlasExtTiles", this.data);
             }
         }
-
     }
 
     public ExtBiomeData getData() {
